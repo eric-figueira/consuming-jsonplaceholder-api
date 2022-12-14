8e04b1c0a62798b8cd1a11750e22d61601e0ff71
ericfigueira <eric.figueira.07@gmail.com>
2022-12-14 09:39:03 -0300

    Installed ejs depency and solved bugs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const rota = require('./routes/route')
+const path = require('path')
 const port = 3000
 
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use('/', rota)
+app.use(express.static(path.join(__dirname,'public')))
 
 
 app.listen(port, () => {
--- src/controller/controller.js
+++ src/controller/controller.js
@@ -7,6 +7,11 @@ const POSTS_URL    = "https://jsonplaceholder.typicode.com/posts"
 const COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments"
 
 
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 exports.render_home = ('/home', async(req, res) => {
     try 
     {
@@ -25,7 +30,26 @@ exports.render_home = ('/home', async(req, res) => {
                 const res_albums = responses[2]["data"]
                 const res_posts  = responses[3]["data"]
 
-                
+                for (let user of res_users) {
+                    for (let i = 0; i < res_posts.length; i++) {
+                        //Every post has an album, so this for loop is valid, since 10 posts have 10 albums and 10 thumbnails
+                        // if the post id is 37, then the album id is 37 as well
+                        let obj = {}
+                        if (res_posts[i]["userId"] == user["id"]) {
+                            obj.username = user["username"]
+                            obj.title = res_posts[i]["title"]
+                            obj.body = res_posts[i]["body"]
+                            if (res_albums[i]["userId"] == user["id"]) {
+                                let photos_from_that_album = res_photos.filter(photo => photo["albumId"] == res_albums[i]["id"])
+                                // Since the api gives 50 photos for each album, we are going to random one of them to be the thumbnail
+                                let index = Math.floor(Math.random() * photos_from_that_album.length);
+                                obj.thumbnailUrl = photos_from_that_album[index]["thumbnailUrl"]
+                            }
+                            final_render_response.push(obj)
+                        }
+                    }
+                }
+                res.render('views/home')
             })
         )
     }
