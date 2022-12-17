6c9681eb7d7b8a6608b3da7fc388f87218038e66
ericfigueira <eric.figueira.07@gmail.com>
2022-12-17 09:36:33 -0300

    Started working on the EJS

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,5 +4,6 @@ const controller = require('../controller/controller')
 
 router.get('/home', controller.render_home)
 router.get('/profile/:username', controller.render_profile)
+router.get('/post/:title', controller.render_post)
 
 module.exports = router
